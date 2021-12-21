7c66ce9ecd100ddd041f32124abd3e5dbae401f5
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-12-21 15:24:25 -0300

    docs(readme) Referencia código produzido

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Hello, World!");
 
 // private - Visível apenas dentro da classe de definição
 // protected - Visível pela classe de definição e suas filhas
-// internal -  Visível para um mesmo Assembly
+// internal - Visível para um mesmo Assembly
 // public - Visível para tudo
 public class Pagamento
 {
