a7cb28e99c19881d7cdcb88e27bda37aa33ffd35
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-12-21 16:39:55 -0300

    feat: Define código da aula 1-13

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -1,53 +1,15 @@
-﻿var pagamento = new Pagamento(DateTime.Now);
+﻿using (var pagamento = new Pagamento()) { }
 
 Console.WriteLine("Hello, World!");
-
-// private - Visível apenas dentro da classe de definição
-// protected - Visível pela classe de definição e suas filhas
-// internal - Visível para um mesmo Assembly
-// public - Visível para tudo
-public class Pagamento
+public class Pagamento : IDisposable
 {
-    // Propriedades - Permite interação antes de obter ou atribuir valor
-    public Pagamento(DateTime vencimento)
+    public Pagamento()
     {
-        Vencimento = vencimento;
-    }
-    public DateTime Vencimento { get; set; }
-    public DateTime DataPagamento { get; private set; }
-    private DateTime _dataAbertura;
-    public DateTime DataAbertura
-    {
-        get { return _dataAbertura; }
-        set { _dataAbertura = value; }
+        Console.WriteLine("Iniciando pagamento");
     }
 
-    // Variáveis - Acesso direto para obter e atribuir valor
-    Address BillingAddress;
-
-    // Métodos
-    public virtual void Pagar() { }
-
-    // Sobrecarga
-    public void Pagar(string numero) { }
-}
-
-public class PagamentoCartao : Pagamento
-{
-    public PagamentoCartao(DateTime vencimento)
-    : base(vencimento)
+    public void Dispose()
     {
-
+        Console.WriteLine("Finalizando pagamento");
     }
-    // Sobrescrita
-    public override void Pagar()
-    {
-        base.Pagar();
-    }
-}
-
-// Tipos Complexos - classes ou structs
-public struct Address
-{
-    string ZipCode;
 }
