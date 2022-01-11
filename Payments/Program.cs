58db24f481291ae00f4aa1c7bf87aedecade894b
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-01-10 21:07:30 -0300

    feat: Define código da aula 1-19

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -1,42 +1,28 @@
 ﻿Console.WriteLine("Hello, World!");
 
-public interface IPagamento
-{
-    DateTime Vencimento { get; set; }
+// Upcast
+var pessoa = new Pessoa();
+pessoa = new PessoaFisica();
+pessoa = new PessoaJuridica();
 
-    void Pagar(double valor);
+// Downcast
+var pessoaFisica = new PessoaFisica();
+pessoaFisica = (PessoaFisica)pessoa;
+
+var pessoaJuridica = new PessoaJuridica();
+pessoaJuridica = (PessoaJuridica)pessoa;
+
+class Pessoa
+{
+    public string Nome { get; set; }
 }
 
-public abstract class Pagamento : IPagamento
+class PessoaFisica : Pessoa
 {
-    public DateTime Vencimento { get; set; }
+    public string Cpf { get; set; }
+}
 
-    public virtual void Pagar(double valor)
-    {
-        throw new NotImplementedException();
-    }
-
-    class PagamentoBoleto : Pagamento
-    {
-        public override void Pagar(double valor)
-        {
-            base.Pagar(valor);
-        }
-    }
-
-    class PagamentoCartaoCredito : Pagamento
-    {
-        public override void Pagar(double valor)
-        {
-            base.Pagar(valor);
-        }
-    }
-
-    class PagamentoApplePay : Pagamento
-    {
-        public override void Pagar(double valor)
-        {
-            base.Pagar(valor);
-        }
-    }
+class PessoaJuridica : Pessoa
+{
+    public string Cnpj { get; set; }
 }
