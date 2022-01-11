e0de9849264588845e1635bbe80f45c1e625bc89
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-01-10 21:35:25 -0300

    feat: Altera comportamento de operadores
    O comportamento dos operadores `==` e `!=` utilizam o método Equals
    provido pela interface IEquatable<T> para comparar objetos.

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -3,6 +3,8 @@ var pessoaB = new Pessoa(1, "Rene");
 
 Console.WriteLine(pessoaA.Equals(pessoaB));
 
+Console.WriteLine(pessoaA == pessoaB);
+
 class Pessoa : IEquatable<Pessoa>
 {
     public Pessoa(int id, string nome)
@@ -10,9 +12,25 @@ class Pessoa : IEquatable<Pessoa>
         Id = id;
         Nome = nome;
     }
-
     public int Id { get; set; }
     public string Nome { get; set; }
 
     public bool Equals(Pessoa? other) => Id == other?.Id;
+
+    public static bool operator !=(Pessoa a, Pessoa b) => !(a == b);
+
+    public static bool operator ==(Pessoa a, Pessoa b)
+    {
+        if (a is null && b is null)
+        {
+            return true;
+        }
+
+        if (a is null || b is null)
+        {
+            return false;
+        }
+
+        return a.Equals(b);
+    }
 }
