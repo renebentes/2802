c584d56fc8cd878c59f9c9f3a944d0a65edf5df5
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-01-17 20:51:14 -0300

    feat: Define código da aula 1-21

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -1,36 +1,9 @@
-﻿var pessoaA = new Pessoa(1, "Rene");
-var pessoaB = new Pessoa(1, "Rene");
+﻿static void RealizarPagamento(double valor) => Console.WriteLine($"Pago o valor de {valor}");
 
-Console.WriteLine(pessoaA.Equals(pessoaB));
+var pagar = new Pagamento.Pagar(RealizarPagamento);
+pagar(25);
 
-Console.WriteLine(pessoaA == pessoaB);
-
-class Pessoa : IEquatable<Pessoa>
+class Pagamento
 {
-    public Pessoa(int id, string nome)
-    {
-        Id = id;
-        Nome = nome;
-    }
-    public int Id { get; set; }
-    public string Nome { get; set; }
-
-    public bool Equals(Pessoa? other) => Id == other?.Id;
-
-    public static bool operator !=(Pessoa a, Pessoa b) => !(a == b);
-
-    public static bool operator ==(Pessoa a, Pessoa b)
-    {
-        if (a is null && b is null)
-        {
-            return true;
-        }
-
-        if (a is null || b is null)
-        {
-            return false;
-        }
-
-        return a.Equals(b);
-    }
+    public delegate void Pagar(double valor);
 }
