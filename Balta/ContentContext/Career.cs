29712874452c9017c3df04bca03995390290e661
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-01-19 19:36:48 -0300

    feat: Define o modelo de carreiras

--- Balta/ContentContext/Career.cs
+++ Balta/ContentContext/Career.cs
@@ -2,5 +2,12 @@ namespace Balta.ContentContext;
 
 public class Career : Content
 {
+    public Career()
+    {
+        Items = new List<CareerItem>();
+    }
 
+    public IList<CareerItem> Items { get; set; }
+
+    public int TotalCourses => Items.Count;
 }
