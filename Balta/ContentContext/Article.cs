6e3edf0bbc2af738eab7ba5885ec87b3ca37f22f
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-01-19 19:50:30 -0300

    feat: Define métodos construtores para tipos de conteúdo

--- Balta/ContentContext/Article.cs
+++ Balta/ContentContext/Article.cs
@@ -2,5 +2,9 @@ namespace Balta.ContentContext;
 
 public class Article : Content
 {
+    public Article(string title, string url)
+        : base(title, url)
+    {
 
+    }
 }
--- Balta/Program.cs
+++ Balta/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Balta.ContentContext;
+
+var articles = new List<Article>();
+articles.Add(new Article("Artigo sobre OOP", "orientacao-a-objetos"));
+articles.Add(new Article("Artigo sobre C#", "csharp"));
+articles.Add(new Article("Artigo sobre .NET", "dotnet"));
+
+foreach (var article in articles)
+{
+    Console.WriteLine(article.Id);
+    Console.WriteLine(article.Title);
+    Console.WriteLine(article.Url);
+}
