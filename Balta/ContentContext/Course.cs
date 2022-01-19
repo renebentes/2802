3d989b1aee4d44de7c8a0c6dc62b8da96fa5b0e3
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-01-19 19:10:57 -0300

    feat: Define modelos de dados internos de cursos

--- Balta/ContentContext/Course.cs
+++ Balta/ContentContext/Course.cs
@@ -2,5 +2,12 @@ namespace Balta.ContentContext;
 
 public class Course : Content
 {
+    public Course()
+    {
+        Modules = new List<Module>();
+    }
 
+    public string Tag { get; set; }
+
+    public IList<Module> Modules { get; set; }
 }
---
+++ Balta/ContentContext/Lecture.cs
@@ -0,0 +1,8 @@
+namespace Balta.ContentContext;
+
+public class Lecture
+{
+    public int Order { get; set; }
+
+    public string Title { get; set; }
+}
