0de332170769b218505fb041dc157f0d44ef7a75
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-01-19 17:40:27 -0300

    feat: Define código da aula 1-26

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -1,12 +1,40 @@
-﻿var payment = new Payment();
+﻿var payments = new List<Payment>();
+payments.Add(new Payment(1));
+payments.Add(new Payment(2));
+payments.Add(new Payment(3));
+payments.Add(new Payment(4));
+payments.Add(new Payment(5));
+payments.Add(new Payment(6));
 
-// Com o var sempre teremos um objeto do tipo List
-var payments = new List<Payment>();
-payments.Add(payment);
+foreach (var p in payments)
+{
+    Console.WriteLine(p.Id);
+}
+
+// Adiciona uma lista à lista
+var paidPayments = new List<Payment>();
+paidPayments.AddRange(payments);
+
+// Pesquisar um elemento - algumas formas
+// .Where - Retorna um IEnumerable
+// .First/.FirstOrDefault - Retorna o elemento, o primeiro dá erro se não achar
+var payment = payments.FirstOrDefault(x => x.Id == 3);
+Console.WriteLine(payment);
+
+// Removendo elementos
+// .RemoveRange
+// .Clear
 payments.Remove(payment);
 
-// Ao usar o tipo explícito, o objeto será limitado ao tipo
-// definido. Só os métodos do tipo serão permitidos
-IEnumerable<string> strings = new List<string>();
+// .Skip - Pula a quantidade de itens informada
+// .Take - Retorna a quantidade de itens informada
 
-class Payment { }
+class Payment
+{
+    public int Id { get; set; }
+
+    public Payment(int id)
+    {
+        Id = id;
+    }
+}
